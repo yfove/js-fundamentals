6c4f2df448ff86e9dac5e47bae4bdf9911bbb809
curtis chat <curtis.ch.chat@gmail.com>
2019-12-05 01:02:25 -0700

    week 2 finishedS

---
+++ week2/lesson1.3/Homework/1.3.js
@@ -0,0 +1,28 @@
+// review
+// -----------------
+
+    //if/ if  else / else
+    if(password === correct) {
+        //show stuff
+    }else if (passwords === oldPasword){
+        //alert they changed their password
+    }else{
+        //reset password link
+    }
+
+    //loop
+    for(let i = 0; i<=10; I++){
+        console.log(i);
+    }
+
+    while(condition === true){
+        if(condition > 10){
+            return;
+        }
+    }
+    //built in function
+    prompt();
+    alert();
+    confirm();
+    Math.max()
+    

ca745b0e477255143dd86d0684c10e8efcc8d148
curtis chat <curtis.ch.chat@gmail.com>
2019-12-08 17:49:22 -0700

    finished week5

--- week4/Level1/levelOne.js
+++ week4/Level1/levelOne.js
@@ -1,13 +1,14 @@
 // Which is bigger?
 // Write a function that takes in two numbers as parameters, and then returns or logs the larger number.
 
-const biggerNumber = (num1, num2) =>{
+const biggerNumber = (num1, num2) => {
     if(num1>num2){
         console.log(`The bigger number is: ${num1}`);
     }else{
-        console.log(`The bigger number is: ${ num2 }`)
+        console.log(`The bigger number is: ${num2}`);
     }
 }
+
 
 biggerNumber(8, 5);
 // 8
@@ -16,20 +17,17 @@ biggerNumber(-5, 10)
 
 //Space Letters
 // Create a function that takes in a string parameter representing some words or a sentence, and returns or logs that string with spaces in between all the characters.
-const spaceLetters = (stringToSplit)=>{
-    const arrayOfStrings = stringToSplit.split("");
+const spaceletters = (stringToSplit) => {
+    const arrayOfStrings = stringsToSplit.split("");
     console.log(arrayOfStrings);
     const spaceStr = arrayOfStrings.join(" ");
     console.log(spaceStr);
 }
-
-spaceLetters("Hello world!");
 // "H e l l o   w o r l d !"
 
 // Find string in array
 // Create a function that takes in two parameters: a string and an array of strings. It should attempt to find the string inside of the array, and return whether the search was successful or not.
-const containsString = (str, array) =>{
-
+const findString = (str, array) => {
     const item = array.indexOf(str);
     return item >= 0;
 }
@@ -41,19 +39,14 @@ containsString('bagel', ['brioche', 'bagel', 'baguette']);
 
 // Count a letter
 // Write a function that that accepts two string arguments, a single letter, and a passage of text to search within. The function should count the number of occurrences of the specified letter within the larger string.
-const letterCount=(string,userLetter)=>{
-    //split the string so it becomes an array
-        stringSplit = string.split('');
-    //use filter, filter will match the letter with the string array and return a new array with all the matches
-            const itMatches = stringSplit.filter(letter => {
-                return letter === userLetter;
-            });
+const letterCount = (string, userletter) => {
+    stringsplit = string.split('');
+        const itMatches = string.split.filter(letter => {
+            return letter === userletter;
+        });
         console.log(itMatches);
-    //use itMatches.length to see the length of the new array created by filter
-        console.log(`The letter "${userLetter}" matches "${string}" ${itMatches.length} times. `);
-    }
+}
 
-letterCount('Hello world!', 'o');
 // 2
 
 //Vowel Count
@@ -83,7 +76,7 @@ countVowels('whrrmyvwlst')
 countVowels('AeIOuy');
 // 5
 
-// Second Highest 
+// Second Highest
 //Create a function that takes in a list of numbers, and returns the second highest number from those numbers.
 
 const secondHighest = (numberArray) => {
@@ -109,116 +102,116 @@ secondHighest([5, 1, 3, 4]);
 
 
 //TIFF's Answers:
-<!DOCTYPE html>
-<html lang="en">
+// <!DOCTYPE html>
+// <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Project One // Level One</title>
-</head>
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Project One // Level One</title>
+// </head>
 
-<body>
-    <script>
-        // LEVEL ONE
+// <body>
+//     <script>
+//         // LEVEL ONE
 
-        // Which is bigger?
-        // Write a function that takes in two numbers as parameters, and then returns or logs the larger number.
-        const biggerNumber = (n1, n2) => {
-            if (n1 >= n2) {
-                return n1;
-            }
-            return n2;
-        }
-        console.log(biggerNumber(100, 20));
-
-
-
-
-        // S p a c e L e t t e r s:
-        // Create a function that takes in a string parameter representing some words or a sentence, and returns or logs that string with spaces in between all the characters.
-        const spaceLetters = (string) => {
-            // split returns array
-            let word = string.split('');
-            return word.join(' ');
-        }
-
-        console.log(spaceLetters("Hello world!"));
+//         // Which is bigger?
+//         // Write a function that takes in two numbers as parameters, and then returns or logs the larger number.
+//         const biggerNumber = (n1, n2) => {
+//             if (n1 >= n2) {
+//                 return n1;
+//             }
+//             return n2;
+//         }
+//         console.log(biggerNumber(100, 20));
 
 
 
 
-        // Find string in array
-        // Create a function that takes in two parameters: a string and an array of strings.It should attempt to find the string inside of the array, and return whether the search was successful or not.
-        const containsString = (string, array) => {
-            if (array.indexOf(string) >= 0) {
-                return true;
-            }
-            return false;
-        }
+//         // S p a c e L e t t e r s:
+//         // Create a function that takes in a string parameter representing some words or a sentence, and returns or logs that string with spaces in between all the characters.
+//         const spaceLetters = (string) => {
+//             // split returns array
+//             let word = string.split('');
+//             return word.join(' ');
+//         }
 
-        console.log(containsString('bagel', ['brioche', 'bagel', 'baguette']));
-
-
-
-
-        // Count a letter
-        // Write a function that that accepts two string arguments, a single letter, and a passage of text to search within.The function should count the number of occurrences of the specified letter within the larger string.
-        const letterCount = (string, letterToCheck) => {
-            const check = string.split('');
-            let count = 0;
-            check.forEach((letter) => {
-                if (letter === letterToCheck) {
-                    count = count + 1;
-                }
-            });
-            return count;
-        }
-
-        console.log(letterCount('Hello world!', 'o'));
+//         console.log(spaceLetters("Hello world!"));
 
 
 
 
-        // Vowel Count
-        // Create a function that counts the number of vowels within a string.It should handle both capitalized and uncapitalized vowels! Ignore the letter ‘Y’.
-        const vowelCount = (string) => {
-            const vowels = ['a', 'e', 'i', 'o', 'u'];
-            const check = string.split('');
-            let count = 0;
-            check.filter((letter) => {
-                if (vowels.indexOf(letter.toLowerCase()) >= 0) {
-                    count = count + 1;
-                }
-            });
-            return count;
-        }
-        console.log(vowelCount('The quick brown fox'));
-        console.log(vowelCount('whrrmyvwlst'));
-        console.log(vowelCount('AeIOuy'));
+//         // Find string in array
+//         // Create a function that takes in two parameters: a string and an array of strings.It should attempt to find the string inside of the array, and return whether the search was successful or not.
+//         const containsString = (string, array) => {
+//             if (array.indexOf(string) >= 0) {
+//                 return true;
+//             }
+//             return false;
+//         }
+
+//         console.log(containsString('bagel', ['brioche', 'bagel', 'baguette']));
 
 
 
 
-        // Second Highest
-        // Create a function that takes in a list of numbers, and returns the second highest number from those numbers.
-        const comparison = (a, b) => {
-            if (a >= b) {
-                return 1;
-            }
-            return -1;
-        }
-        const secondHighest = (array) => {
-            // order array
-            // then take length - 2
-            array = array.sort(comparison);
-            const indexSecondLowest = array.length - 2;
-            return array[indexSecondLowest];
+//         // Count a letter
+//         // Write a function that that accepts two string arguments, a single letter, and a passage of text to search within.The function should count the number of occurrences of the specified letter within the larger string.
+//         const letterCount = (string, letterToCheck) => {
+//             const check = string.split('');
+//             let count = 0;
+//             check.forEach((letter) => {
+//                 if (letter === letterToCheck) {
+//                     count = count + 1;
+//                 }
+//             });
+//             return count;
+//         }
 
-        }
-        console.log(secondHighest([5, 8, 9, 2, -8]));
+//         console.log(letterCount('Hello world!', 'o'));
 
-    </script>
-</body>
-</html>
+
+
+
+//         // Vowel Count
+//         // Create a function that counts the number of vowels within a string.It should handle both capitalized and uncapitalized vowels! Ignore the letter ‘Y’.
+//         const vowelCount = (string) => {
+//             const vowels = ['a', 'e', 'i', 'o', 'u'];
+//             const check = string.split('');
+//             let count = 0;
+//             check.filter((letter) => {
+//                 if (vowels.indexOf(letter.toLowerCase()) >= 0) {
+//                     count = count + 1;
+//                 }
+//             });
+//             return count;
+//         }
+//         console.log(vowelCount('The quick brown fox'));
+//         console.log(vowelCount('whrrmyvwlst'));
+//         console.log(vowelCount('AeIOuy'));
+
+
+
+
+//         // Second Highest
+//         // Create a function that takes in a list of numbers, and returns the second highest number from those numbers.
+//         const comparison = (a, b) => {
+//             if (a >= b) {
+//                 return 1;
+//             }
+//             return -1;
+//         }
+//         const secondHighest = (array) => {
+//             // order array
+//             // then take length - 2
+//             array = array.sort(comparison);
+//             const indexSecondLowest = array.length - 2;
+//             return array[indexSecondLowest];
+
+//         }
+//         console.log(secondHighest([5, 8, 9, 2, -8]));
+
+//     </script>
+// </body>
+// </html>
